f79df5a86b78012e9be385040f1c1d50bfb5d565
Andrii Traitaka <traitacka@gmail.com>
2026-09-17 14:28:04 +0300

    лабораторна 3

--- index.php
+++ index.php
@@ -4,13 +4,26 @@ $appName = "Task Manager";
 $taskTitle = "Оголошення змінних PHP";
 $taskTimeEstimate = 2;
 
+$isCompleted = true;
+
 ?>
+
 <!DOCTYPE html>
 <html lang="uk">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $appName ?></title>
+    <style>
+        .task-done {
+            color: green;
+            outline: 1px;
+            outline-color: black;
+        }
+        .task-pending {
+            color: gray;
+        }
+    </style>
 </head>
 <body>
     <header>
@@ -21,6 +34,7 @@ $taskTimeEstimate = 2;
             <li><?= $taskTitle ?></li>
             <li><?= $taskTimeEstimate ?></li>
         </ul>
+        <p class="<?= $isCompleted ? 'task-done' : 'task-pending' ?>"><?php if ($isCompleted): ?>✔️ Виконано<?php else: ?>🕒 В процесі<?php endif; ?></p>
     </main>
 </body>
 </html>
